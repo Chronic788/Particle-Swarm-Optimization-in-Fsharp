498b173c92f78e927093fee89d94cb91718d0e2c
Jared Nelsen <jnelsen788@gmail.com>
2020-07-22 16:03:20 -0400

    Wrote the code to update the particle velocities. Getting some errors

--- pso.fsx
+++ pso.fsx
@@ -5,16 +5,65 @@
     See this cheat sheet for F# basics: https://dungpa.github.io/fsharp-cheatsheet/
 *)
 
+// Utilities
+let random = System.Random()
+
 // Learning factors
 // -------------------------------------------------------------------------------------------------------
 // - Research shows that c1 and c2 should generally be equal and suggest that the best value is '2'
-let c1 = 2
-let c2 = 2
+let c1 = 2.0
+let c2 = 2.0
 
 // Global best
 let globalBest = 9999999999999999.0
 
 // Types
 type Position = {x: float; y: float}
-type Particle = {position: Position; velocity: float}
-type Swarm = {particles: Particle []}
+type Particle = {position: Position; xVelocity: float; yVelocity: float; personalBest : Position}
+type Swarm = {particles: list<Particle>; globalBest: Particle}
+
+let rec updateParticleVelocities (particles : list<Particle>, updatedParticles : list<Particle>, swarm : Swarm) =
+
+    // Define the velocity update functions for the x and y axis
+    let updateVelocities (particle : Particle) =
+
+        let updateXVelocity (particle : Particle) =
+            let presentVelocityTerm = 
+                particle.xVelocity
+            let personalBestTerm = 
+                c1 * random.NextDouble() * (particle.personalBest.x - particle.position.x)
+            let globalBestTerm = 
+                c2 * random.NextDouble() * (swarm.globalBest.position.x - particle.position.x)
+
+            //Is this how to update a value?
+            particle.xVelocity <| presentVelocityTerm + personalBestTerm + globalBestTerm
+            particle
+
+        let updateYVelocity (particle : Particle) =
+            let presentVelocityTerm = 
+                particle.yVelocity
+            let personalBestTerm = 
+                c1 * random.NextDouble() * (particle.personalBest.y - particle.position.y)
+            let globalBestTerm = 
+                c2 * random.NextDouble() * (swarm.globalBest.position.y - particle.position.y)
+
+            // Is this how to update a value??
+            particle.yVelocity <| presentVelocityTerm + personalBestTerm + globalBestTerm
+            particle
+
+        let update =
+            particle
+            |> updateXVelocity
+            |> updateYVelocity
+
+        update particle
+
+    if particles.Length = 0 then
+        updatedParticles
+    else
+        updateParticleVelocities particles.Tail updatedParticles::(updateVelocities particles.Head) swarm
+        
+
+    
+    
+// Bounds check for velocity update
